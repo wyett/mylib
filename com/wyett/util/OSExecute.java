a84e00ae809efcfb0ecaedfbd9dc8dab78d0a9d3
wyett <wyettlei@hotmail.com>
2019-04-29 18:33:50 +0800

    OSExecute file

---
+++ com/wyett/util/OSExecute.java
@@ -0,0 +1,32 @@
+// com/wyett/util/OSExecute.java
+package com.wyett.util;
+import java.io.*;
+
+public class OSExecute {
+    public static void command(String command){
+        boolean err = false;
+        try {
+            Process process = 
+                new ProcessBuilder(command.split(" ")).start();
+            BufferedReader results = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+            String s;
+            while((s = results.readLine()) != null)
+                System.out.println(s);
+            BufferedReader errors = new BufferedReader(
+                new InputStreamReader(process.getErrorStream()));
+            while((s = errors.readLine()) != null){
+                System.out.println(s);
+                err = true;
+            }
+        }catch(Exception e){
+            if(!command.startsWith("CMD /C"))
+                command("CMD /C " + command);
+            else
+                throw new RuntimeException(e);
+        } 
+        if(err)
+            throw new OSExecuteException("Errors executing " +
+                command);
+    }
+}
---
+++ com/wyett/util/OSExecuteException.java
@@ -0,0 +1,6 @@
+// com/wyett/util/OSExecuteException.java
+package com.wyett.util;
+
+public class OSExecuteException extends RuntimeException {
+    public OSExecuteException(String why) {super(why);}
+}
