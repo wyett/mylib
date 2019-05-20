091765cdb310c1bd835c796e5864d91763e201d4
wyett <wyettlei@hotmail.com>
2019-05-20 15:39:17 +0800

    add class Enums

---
+++ com/wyett/util/Enums.java
@@ -0,0 +1,15 @@
+// com/wyett/util/Enums.java
+package com.wyett.util;
+import java.util.*;
+
+public class Enums {
+    private static Random rand = new Random(47);
+    // <T extends Enum<T>>表示T是一个enum实例。
+    // Class<T>作为参数，就可以利用Class对象得到enum实例的数组。
+    public static <T extends Enum<T>> T random(Class<T> ec) {
+        return random(ec.getEnumConstants());
+    }
+    public static <T> T random(T[] values) {
+        return values[rand.nextInt(values.length)];
+    }
+}
